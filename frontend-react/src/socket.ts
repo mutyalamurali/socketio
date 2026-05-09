126ecbe9d35e87b4669cb49397e3c6da1cc22ff4
Murali Mutyala <murali@atforte.com>
2026-05-09 23:09:07 +0530

    [DELETE] frontend-react typescript files deletes

--- frontend-react/src/socket.ts
+++
@@ -1,9 +0,0 @@
-import { io, Socket } from 'socket.io-client';
-
-const SOCKET_URL = 'http://localhost:4000'; // Adjust this for production
-
-const socket: Socket = io(SOCKET_URL, {
-  autoConnect: true, // We will connect manually
-});
-
-export default socket;
